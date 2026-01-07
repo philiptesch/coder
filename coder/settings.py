e384aab4030867257eb051487f0604e219a74b92
Philip <philiptesch99@gmail.com>
2026-01-07 11:39:10 +0100

    add coresheaders in settings.py

--- coder/settings.py
+++ coder/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     'corsheaders', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,4 @@ REST_FRAMEWORK = {
 
 
 }
+CORS_ALLOW_ALL_ORIGINS = True
